c09b62ff1bd218617cdb1e786dc894cdae968a6e
pravatjavaarena <pravatkumar772@gmail.com>
2022-08-08 01:11:28 +0530

    Book.java modified at 1.11 pm

--- Book.java
+++ Book.java
@@ -6,6 +6,7 @@ public static void main(String args[]){
  System.out.println("Hello pravat kumar samal"); 
  
  System.out.println("Institute of management and information technology");
+ System.out.println("I have completed my graduation from BPUT");
 
 }
 
