9557492f88be1f53eda134377ab15b7b2585abfe
pravatjavaarena <93695670+pravatjavaarena@users.noreply.github.com>
2022-08-08 01:03:41 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 
  System.out.println("Hello pravat kumar samal"); 
  
- System.out.println("Deepak kumar samal");
+ System.out.println("Institute of management and information technology");
 
 }
 
